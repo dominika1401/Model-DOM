57ffb181a5e9ae3688d31fcdcdb1ded8f1e92ad9
dominika1401 <121903073+dominika1401@users.noreply.github.com>
2023-02-27 10:04:53 +0100

    zadania

--- 1-lista-miast/js/main.js
+++ 1-lista-miast/js/main.js
@@ -1,5 +1,12 @@
 let cities = ['Warsaw', 'Berlin', 'Paris', 'London', 'New York', 'Tokio', 'Moscow'];
+let lista =  document.createElement('ol');
 for (let i = 0; i < cities.length; i++) {
-    document.querySelector('body').innerHTML='<ol><li class="city">'+cities[i-6]+'</li><li class="city">' +cities[i-5]+ '</li><li class="city">'+cities[i-4]+'</li><li class="city">'+ cities[i-3]+'</li><li class="city">'+cities[i-2]+'</li><li class="city">'
-    +cities[i-1]+'</li><li class="city">'+cities[i]+'</li></ol>';
+    let newElem = document.createElement('li');
+    newElem.innerHTML = cities[i];
+    document.body.appendChild(lista);
+    lista.appendChild(newElem);
+    let attrNode = document.createAttribute('class');
+    attrNode.value = 'city';
+    newElem.setAttributeNode(attrNode);
 }
+console.log(document)
--- 6-wyplaty-premie-dodatkowe/js/main.js
+++ 6-wyplaty-premie-dodatkowe/js/main.js
@@ -2,20 +2,34 @@ let button = document.getElementById('oblicz');
 const wyplata = (e) => {
     let pracownicy = document.querySelectorAll('[id^="pracownik"]');
     for (let i = 0; i < pracownicy.length; i++) {
-        let time = pracownicy[i].querySelector(".czas").value;
+        let time = pracownicy[i].querySelector(".czas");
         let gotowka = pracownicy[i].querySelector(".stawka").value;
         let salary = 0
         let overTime = 0
 
-        if (time > 160) {
-            overTime = time - 160;
-        } else if (time < 100) {
+        if (time.value > 160) {
+            overTime = time.value - 160;
+        } else if (time.value < 100) {
             pracownicy[i].style.background = "red";
         }
-        salary = time * gotowka + overTime * gotowka * 2;
-        console.log(pracownicy[i].querySelector(".wyplata"))
+        salary = time.value * gotowka + overTime * gotowka * 2;
+        // console.log(pracownicy[i].querySelector(".wyplata"))
 
         pracownicy[i].querySelector(".wyplata").innerText = salary;
     }
+    let najlepsiPracownicy = document.querySelector('#najlepsi-pracownicy');
+
+    let timeArray = [];
+    let time = document.querySelectorAll(".czas");
+
+    for (let i = 0; i < time.length; i++) {
+        console.log(time[i])
+        timeArray[i] = time[i].value + " " + pracownicy[i].querySelector('.pracownik').textContent;
+
+    }
+    console.log(timeArray);
+    timeArray.sort((a,b) => {
+        console.log(+a);
+    })
 }
 pracownicy.addEventListener('click', wyplata);
